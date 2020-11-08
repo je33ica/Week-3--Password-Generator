3471f8f0032b5eefe22bd722ef8c26ff36e8e932
je33ica <je331687@gmail.com>
2020-11-08 14:00:08 +0000

    const variables aaigned from HTML

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-javascript
+  //all the DOM elements we will need - ref from HTML
+  const resultEl = document.getElementById("result");
+  const lengthEl = document.getElementById("length");
+    // checkboxes 
+  const lowerEl = document.getElementById("lower");
+  const upperEl = document.getElementById("upper");
+  const numberEl = document.getElementById("number");
+  const specialEl = document.getElementById("special");
+  const generateEl = document.getElementById("generate");
